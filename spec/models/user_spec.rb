746300c9475a11968158cccd9a63aa38bfb1f561
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-08-27 01:43:03 +0000

    Completed tests for authenticate_with_credentials

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,128 +2,158 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  context 'password match' do
-    user = User.new(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hogehoge')
-    user.valid?
-    it 'password match' do
-      expect(user.errors[:password_confirmation]).not_to be_present
+  describe 'Validations' do
+    it "is valid" do
+      user = User.new(
+        first_name: 'first_name',
+        last_name: 'last_name',
+        email: 'test2@test.com',
+        password: 'hogehoge',
+        password_confirmation: 'hogehoge'
+      )
+      expect(user).to be_valid
     end
-  end
 
-  context "password don't match" do
-    user = User.new(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hoge')
-    user.valid?
+    it "email is missing" do
+      user = User.new(email: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:email]).to include("can't be blank")
+  
+      user.email = 'test@test.com' # valid state
+      user.valid?
+      expect(user.errors[:email]).not_to include("can't be blank")
+    end
+
+    it "first name is missing" do
+      user = User.new(first_name: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:first_name]).to include("can't be blank")
+  
+      user.first_name = 'first_name' # valid state
+      user.valid? 
+      expect(user.errors[:first_name]).not_to include("can't be blank")
+    end
+
+    it "last name is missing" do
+      user = User.new(last_name: nil)
+      expect(user).to be_invalid
+      expect(user.errors[:last_name]).to include("can't be blank")
+  
+      user.last_name = 'last_name' # valid state
+      user.valid? 
+      expect(user.errors[:last_name]).not_to include("can't be blank")
+    end
+
     it "password don't match" do
+      user = User.new(
+        first_name: 'first_name',
+        last_name: 'last_name',
+        email: 'test2@test.com',
+        password: 'hogehoge',
+        password_confirmation: 'hoge'
+      )
+      user.valid?
       expect(user.errors[:password_confirmation]).to be_present
     end
+
+    it 'email must be unique' do
+      user = User.new
+      user.first_name = 'first_name'
+      user.last_name = 'last_name'
+      user.email = 'test@test.com'
+      user.password = 'hogehoge'
+      user.password_confirmation = 'hogehoge'
+
+      user.save
+    
+      u = User.new
+      u.first_name = 'first_name'
+      u.last_name = 'last_name'
+      u.email = 'test@test.com'
+      u.password = 'hogehoge'
+      u.password_confirmation = 'hogehoge'
+      u.save
+    
+      expect(u.errors[:email].first).to eq('has already been taken')
+    end
+
+    it 'password length less than 5 characters is invalid' do
+      user = User.new
+      user.first_name = 'first_name'
+      user.last_name = 'last_name'
+      user.email = 'test@test.com'
+      user.password = '1234'
+      user.password_confirmation = '1234'
+      expect(user).to be_invalid
+    end
+
+    it 'password length must be at-least 5 characters' do
+      user = User.new
+      user.first_name = 'first_name'
+      user.last_name = 'last_name'
+      user.email = 'test@test.com'
+      user.password = '12345'
+      user.password_confirmation = '12345'
+      expect(user).to be_valid
+    end
   end
+  
+  describe '.authenticate_with_credentials' do
+    it 'should pass with valid credentials' do
+      user = User.new(
+        first_name: 'first_name',
+        last_name: 'last_name',
+        email: 'test@test.com',
+        password: 'hogehoge',
+        password_confirmation: 'hogehoge'
+      )
+      user.save
 
-  it 'email must be unique' do
-    user = User.new(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hogehoge')
-    user.save
-  
-    u = User.new(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hogehoge')
-    u.save
-  
-    expect(u.errors[:email].first).to eq('has already been taken')
+      user = User.authenticate_with_credentials('test@test.com', 'hogehoge')
+      expect(user).not_to be(nil)
+    end
+
+    it 'should not pass with invalid credentials' do
+      user = User.new(
+        first_name: 'first_name',
+        last_name: 'last_name',
+        email: 'test@test.com',
+        password: 'hogehoge',
+        password_confirmation: 'hogehoge'
+      )
+      user.save
+
+      user = User.authenticate_with_credentials('test@test.com', 'hogehoge3')
+      expect(user).to be(nil)
+    end
+
+    it 'should pass even with spaces present in email' do
+      user = User.new(
+        first_name: 'first_name',
+        last_name: 'last_name',
+        email: 'test@test.com',
+        password: 'hogehoge',
+        password_confirmation: 'hogehoge'
+      )
+      user.save
+
+      user = User.authenticate_with_credentials('  test@test.com  ', 'hogehoge')
+      expect(user).not_to be(nil)
+    end
+
+    it 'should pass even with caps present in email' do
+      user = User.new(
+        first_name: 'first_name',
+        last_name: 'last_name',
+        email: 'test@test.com',
+        password: 'hogehoge',
+        password_confirmation: 'hogehoge'
+      )
+      user.save
+
+      user = User.authenticate_with_credentials('tesT@tEst.com', 'hogehoge')
+      expect(user).not_to be(nil)
+    end
   end
-
-  it 'password length less than 5 characters is invalid' do
-    user = User.new(email: 'bugs', password: '1234')
-
-    result = user.save
-
-    expect(result).to be(false)
-  end
-
-  it 'password length must be at-least 5 characters' do
-    user = User.new(email: 'bugs', password: '12345')
-  
-    result = user.save
-  
-    expect(result).to be(true)
-  end
-  
-  
-
-  # def user_params
-  #   params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  # end
-
-  # def create
-  #   user = User.new(user_params)
-  #   if user.save
-  #     session[:user_id] = user.id
-  #     redirect_to '/'
-  #   else
-  #     redirect_to '/signup'
-  #   end
-  # it "is valid" do
-  #   @product = Product.new
-  #   @cat = Category.new
-  #   @cat.name = 'Test'
-  #   @product.name = 'Test' # invalid state
-  #   @product.price_cents = 12311
-  #   @product.quantity = 3
-  #   @product.category = @cat
-  #   expect(@product.valid?).to be true
-  # end
-
-  # it "name presence" do
-  #   @product = Product.new
-  #   @product.name = nil # invalid state
-  #   @product.valid?
-  #   expect(@product.errors[:name]).to  include("can't be blank")
-
-  #   @product.name = 'test' # valid state
-  #   @product.valid? 
-  #   expect(@product.errors[:name]).not_to  include("can't be blank")
-  # end
-
-  # it "price_cents presence" do
-  #   @product = Product.new
-  #   @product.price_cents = nil # invalid state
-  #   @product.valid?
-  #   expect(@product.errors[:price_cents]).to  include("is not a number")
-
-  #   @product.price_cents = 12311 # valid state
-  #   @product.valid? 
-  #   expect(@product.errors[:price_cents]).not_to  include("can't be blank")
-  # end
-
-  # it "quantity" do
-  #   @product = Product.new
-  #   @product.quantity = nil # invalid state
-  #   @product.valid?
-  #   expect(@product.errors[:quantity]).to  include("can't be blank")
-
-  #   @product.quantity = 3 # valid state
-  #   @product.valid? 
-  #   expect(@product.errors[:quantity]).not_to  include("can't be blank")
-  # end
-
-  # it "has category_id" do
-  #   @cat = Category.new
-  #   @product = Product.new
-  #   @product.category = nil # invalid state
-  #   @product.valid?
-  #   expect(@product.errors[:category]).to  include("can't be blank")
-
-  #   @product.category = @cat # valid state
-  #   @product.valid? 
-  #   expect(@product.errors[:category]).not_to  include("can't be blank")
-  # end
-
-  #   It must be created with a password and password_confirmation fields
-  #       These need to match so you should have an example for where they are not the same
-  #       These are required when creating the model so you should also have an example for this
-  #   Emails must be unique (not case sensitive; for example, TEST@TEST.com should not be allowed if test@test.COM is in the database)
-  #   Email, first name, and last name should also be required
-
-  #   t.string   "name"
-  #   t.string   "email"
-  #   t.string   "password_digest"
-  # end
-
 
 end
