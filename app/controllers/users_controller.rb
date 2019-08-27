eaa33f202f28a6f63286674b7371b977d4952eeb
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-08-27 23:26:22 +0000

    Fixed bug with strip and downcase

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    user_params[:email] = user_params[:email].downcase.strip
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email.downcase.strip, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
   
 
